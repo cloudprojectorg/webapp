46117f4efa43f798694a08567ad3be729443f557
Anuja-Shinde-02 <114773116+Anuja-Shinde-02@users.noreply.github.com>
2023-10-05 11:57:47 -0400

    Committing to the main branch of organisation (#1)
    * commit 1
    
    * commit 2
    
    * Update README.md
    
    * Update README.md
    
    * commit 3
    
    * commit 3
    
    * Update server.js
    
    * commit 5

---
+++ Controller/assignmentController.js
@@ -0,0 +1,44 @@
+const { Assignment } = require('../Models/Assignment');
+const sequelize = require('../Models/db');
+
+exports.createAssignment = async (req, res) => {
+    try {
+        const {
+            name, points, num_of_attempts, deadline
+        } = req.body;
+
+        const userId = req.user.id;
+
+        // console.log(req.body)
+
+        // 400 Bad Request for invalid input
+        if (!name || !points || !num_of_attempts || !deadline) {
+            return res.status(400).json({ message: 'Invalid input: All fields are required' });
+        }
+
+        // 401 Unauthorized if user id is not present
+        if (!req.user || !req.user.id) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        // const userId = req.user.id;
+        // console.log('In Create assignment')
+        // console.log(req.body)
+        // console.log('----')
+        // console.log(userId)
+        
+        const newAssignment = await sequelize.models.Assignment.create({
+            name,
+            points,
+            num_of_attempts,
+            deadline,
+            userId
+        });
+
+        res.status(201).json({ message: 'Assignment created successfully', data: newAssignment });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+
---
+++ Controller/getAssignmentById.js
@@ -0,0 +1,60 @@
+const  Assignment1  = require('../Models/Assignment');
+const User = require('../Models/User');  // Using the model file you've shared
+const sequelize = require('../Models/db');
+
+exports.getAssignmentById = async (req, res) => {
+    try {
+        const { id } = req.query;
+        const userId = req.user.id;
+        console.log(id)
+    
+        const attributes = Assignment1.rawAttributes;
+        const fields = Object.keys(attributes);
+        console.log(fields);
+        // console.log('Hello')
+
+        const assg = await Assignment1.findOne({where: { id }, 
+        // const assg = await Assignment1.findOne({where: { id: id,userId: userId}, 
+        include: [{
+                model: User,
+                as: 'user', // This should match the alias in your associations
+                attributes: ['id', 'first_name', 'last_name', 'email'] // Choose the user attributes you want to fetch
+            }]
+        })
+
+        // console.log('hello')
+        console.log(assg)
+            
+        // Check if assignment exists
+        if (!assg) {
+            return res.status(404).json({ message: 'Assignment not found' });
+        }
+        
+
+        // Structure the response with assignment and user details
+        const responseDetails = {
+            status: 'success',
+            message: 'Assignment fetched successfully',
+            data: {
+                id: assg.id,
+                name: assg.name,
+                points: assg.points,
+                numOfAttempts: assg.num_of_attempts,
+                deadline: assg.deadline,
+                assignmentCreated: assg.assignment_created,
+                assignmentUpdated: assg.assignment_updated,
+                userId: assg.userId,
+                useremail: assg.user.email,
+                userFName: assg.user.first_name,
+                userLName: assg.user.last_name // This will have the associated user details
+            }
+        };
+
+        console.log(responseDetails)
+
+        // Return the structured response
+        res.status(200).json(responseDetails);
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
