a4ac28eb598cddeb617e3f42fe39f409ea313af1
Anuja-Shinde-02 <shinde.anuj@northeastern.edu>
2023-10-20 13:29:59 -0400

    ami packer

--- database/database.js
+++ database/database.js
@@ -51,20 +51,6 @@ const createDatabase = async () => {
   }
 };
 
-// const initializeModels = async () => {
-//   try {
-//     const UserModel = require('../Models/user')(sequelize);
-//     const AssignmentModel = require('../Models/assignment')(sequelize);
-//        await sequelize.sync({ alter: true })
-//        csvLoader('opt/users.csv')
-//       // await sequelize.sync({ alter: true }).then(()=>csvLoader1('opt/users.csv')); 
-//       console.log('Models synchronized with database.');
-//   } catch (err) {
-//       console.error(`Failed to synchronize models: ${err}`);
-//   }
-// };
-
-
 const initializeModels = async () => {
 
   try {
